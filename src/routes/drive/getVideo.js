eba1f64ccfc6482e53b7a648f9425452f9e324b6
minhpg <minhpg@minhpgs-Mac-mini.local>
2021-08-29 21:18:04 +0700

    commit

--- src/routes/drive/getVideo.js
+++ src/routes/drive/getVideo.js
@@ -11,7 +11,7 @@ module.exports = async (req,res) => {
             error: video.error,
             error_message: video.error_message,
         }
-        if(!video.processing&&!error){
+        if(!video.processing&&!video.error){
             video.embed = `${process.env.HOST}/api/iframe/${video._id}`,
             video.playlist = `${process.env.HOST}/api/m3u8/${video._id}/master.m3u8`
         }
