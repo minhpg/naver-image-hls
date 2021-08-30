e5f74ad83ec6fb7c9c80eb59725de519aeae429c
minhpg <minhpg@minhpgs-Mac-mini.local>
2021-08-30 08:50:34 +0700

    commit

--- static/init.js
+++ static/init.js
@@ -1,5 +1,5 @@
 const initPlayer = (link) =>  {
-    let player = jwplayer("player");
+    let player = jwplayer("jwplayer");
     let object = {
         playbackRateControls: true,
         controls: true,
@@ -10,30 +10,8 @@ const initPlayer = (link) =>  {
         file: link,
         type: "hls",
         preload: "auto",
-        skin: {
-            name:'custom',
-            url: '/static/custom.css',
-        }
     };
     player.setup(object);
-    player.addButton("/static/icons/skip-forward.svg", "Skip Intro", () => {
-        skip_time = player.getPosition() + 90;
-        player.seek(skip_time)
-    }, "skipButton");
-    player.addButton("/static/icons/forward.svg", "FF 5s", () => {
-        player.seek(player.getPosition() + 5);
-    }, "Tua tiếp 5s");
-    player.addButton("/static/icons/backward.svg", "RW 5s", () => {
-        player.seek(player.getPosition() - 5);
-    }, "Tua lại 5s");
-    // if (Hls.isSupported() && p2pml.hlsjs.Engine.isSupported()) {
-    //     var engine = new p2pml.hlsjs.Engine();
-    //     jwplayer_hls_provider.attach();
-    //     p2pml.hlsjs.initJwPlayer(player, {
-    //         liveSyncDurationCount: 7, // To have at least 7 segments in queue
-    //         loader: engine.createLoaderClass(),
-    //     });
-    // }
     player.on('ready', () => {
         console.log(player)
     });
