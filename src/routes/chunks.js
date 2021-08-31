47d54d4ac10f117827dc9dce8e07e23fbf4f1c68
minhpg <minhpg@minhpg-mac-mini.lan>
2021-08-31 16:15:09 +0700

    commit

--- src/routes/chunks.js
+++ src/routes/chunks.js
@@ -13,7 +13,6 @@ const decodeUrl = (str) => {
 module.exports = async (req, res) => {
     res.setHeader('access-control-allow-origin', process.env.CORS_DOMAIN || '*')
     res.setHeader('content-type', 'text/plain')
-    console.log(req.params.url)
     redisClient.get(req.params.url, async (err, data) => {
         try {
             if (err) {
