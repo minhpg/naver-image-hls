c527c596f7fe5996a1773eff6a415cd99c34f26e
minhpg <67231681+minhpg@users.noreply.github.com>
2021-09-08 10:24:57 +0700

    commit

--- test.js
+++
@@ -1,3 +0,0 @@
-const imageProxy = require('./src/google-drive-api/imageProxyV3')
-require('dotenv').config()
-imageProxy('/MjAyMTA4MjhfOTQg/MDAxNjMwMTQ4Njc0MTMy.Us_PchxoNQ69QWZEQO_XaxGFEzuaYqq9eB-QKjCQES4g.pcvwc2ipZhLM0yiTtoLX8opl2SujjNf6SRJPcWhVN7og.PNG/video625.png')
