e3d5745a4a0fb4492b21b369d0d7c41b5c29b4da
minhpg <67231681+minhpg@users.noreply.github.com>
2021-09-13 11:08:10 +0700

    commit

--- static/init.js
+++ static/init.js
@@ -1,6 +1,7 @@
 const initPlayer = (link) =>  {
     let player = jwplayer("player");
     let object = {
+        key: "W7zSm81+mmIsg7F+fyHRKhF3ggLkTqtGMhvI92kbqf/ysE99",
         playbackRateControls: true,
         controls: true,
         volume: 75,
@@ -11,6 +12,16 @@ const initPlayer = (link) =>  {
         type: "hls",
         preload: "auto",
     };
+    player.addButton("/dist/svg/skip-forward.svg", "Skip OP/ED", function() {
+        skip_time = player.getPosition() + 90;
+        player.seek(skip_time)
+    }, "skipButton");
+    player.addButton("/dist/svg/forward.svg", "Tua tiếp 5s", function() {
+        player.seek(player.getPosition() + 5);
+    }, "Tua tiếp 5s");
+    player.addButton("/dist/svg/backward.svg", "Tua lại 5s", function() {
+        player.seek(player.getPosition() - 5);
+    }, "Tua lại 5s");
     player.setup(object);
     if (Hls.isSupported() && p2pml.hlsjs.Engine.isSupported()) {
         var engine = new p2pml.hlsjs.Engine();
