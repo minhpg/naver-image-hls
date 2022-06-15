ff9dba517836aafbab760f2c2a809e54324d80b9
minhpg <67231681+minhpg@users.noreply.github.com>
2022-06-15 23:56:43 +0700

    fix

--- src/discord-api/proxy.js
+++ src/discord-api/proxy.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 const upload = async (image_url, authorization, channel_id) => {
 
     const payload_json = {
-        'content' : image_url
+        'content': image_url
     }
 
     const headers = {
@@ -12,20 +12,24 @@ const upload = async (image_url, authorization, channel_id) => {
     }
 
     const url = `https://discord.com/api/v9/channels/${channel_id}/messages`
-
+    while (true) {
         const response = await axios(
-            {method: 'POST',
-            url,
-            data: payload_json,
-            headers: headers,
-        })
+            {
+                method: 'POST',
+                url,
+                data: payload_json,
+                headers: headers,
+            })
         const data = response.data
         console.log(response.data)
         const embeds = data.embeds
-        if(!embeds) throw new Error('proxy failed!')
+        if (!embeds) throw new Error('proxy failed!')
         else {
-            return embeds[0].thumbnail.proxy_url
+            if (embeds[0].thumbnail) return embeds[0].thumbnail.proxy_url
+
         }
+    }
+
 
 }
 
