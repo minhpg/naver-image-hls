d4a1a4fbf725fb898729998a34283f2e74ff8c6c
minhpg <67231681+minhpg@users.noreply.github.com>
2022-06-15 22:57:11 +0700

    fix

--- src/discord-api/proxy.js
+++ src/discord-api/proxy.js
@@ -14,6 +14,8 @@ const upload = async (image_url, authorization, channel_id) => {
     const headers = {
         authorization: authorization,
         'Content-Type': `multipart/form-data; boundary=${form._boundary}`,
+        'Cache-Control': 'no-cache'
+
     }
 
     const url = `https://discord.com/api/v9/channels/${channel_id}/messages`
