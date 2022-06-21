ef9190fbd6d7859d2bcb1b04daeb5de7a64bee8f
minhpg <67231681+minhpg@users.noreply.github.com>
2022-06-21 15:16:54 +0700

    fix

--- src/discord-api/proxy.js
+++ src/discord-api/proxy.js
@@ -23,7 +23,9 @@ const proxy = async (image_url, authorization, channel_id) => {
         const data = response.data
         console.log(response.data)
         const embeds = data.embeds
-        if (embeds.length==0) continue //throw new Error('proxy failed!')
+        if (embeds.length==0) {
+            await new Promise(r => setTimeout(r, 3000));
+            continue} //throw new Error('proxy failed!')
         else {
             if (embeds[0].thumbnail) return embeds[0].thumbnail.proxy_url
         }
@@ -32,5 +34,7 @@ const proxy = async (image_url, authorization, channel_id) => {
 
 }
 
+
+proxy('http://post-phinf.pstatic.net/MjAyMjA2MjFfMTA2/MDAxNjU1Nzk5MDM2NDQ0.FKsFgosBNK2DFUp9efvN6pudCprw9n4v2wzaLQy9ojQg.iVbk5Ovc9tBZ-AdT-WUGcJsN07KVisrJHcEUW6MpsBsg.PNG/video0.jpeg','OTE5ODMxODQxMTIzNTAwMDUy.YbbiXw.TUjkhscQIF0s6bIulzcvKYs6QY4','919832247039848471')
 
 module.exports = proxy
